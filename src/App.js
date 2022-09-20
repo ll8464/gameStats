962aafd66a486f6e5afa233656bde8f821891a70
Lee Lacey <leelacey128@gmail.com>
2022-09-20 15:52:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav></nav>
       </header>
+      <section className="Main">
+        <div className="Card">
+          <section id="hero">Image</section>
+          <section id="stats">Stat</section>
+          <section id="users">User</section>
+        </div>
+      </section>
+      <footer>Foot</footer>
     </div>
   );
 }
