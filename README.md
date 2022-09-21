e8abe045d69478bb7db203973a3a80fbbf4e36c4
Lee Lacey <leelacey128@gmail.com>
2022-09-21 08:01:40 -0500

    Starts Achievement Functionality

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 
 This react application allows for searching for steam game stats utilizing the steam API.
 
-Technologies/Resources Used:
+### Technologies/Resources Used:
+
 Unsplashed API is used for the randomly generated backgrounds (all gaming related, of course).
 React framework
 
 ## Purpose of Project
 
 The overall goal is to allow users to easily access interesting data about their favorite games.
+
+## Type of Data that can be Accessed
+
+### Possible inclusions
+
+Display the total number of achievements a searched game has.
+Change Achievement Names to splice out all underscores to improve readability.
+
+### Bugs and Errors
+
+SearchIcon not displaying - could be a corrupted file.
--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
 import "./App.css";
+import "./Achievements.js";
+
+let achievements = {
+  fetchAchievements: function (game) {
+    fetch(
+      "http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=" +
+        game +
+        "&format=json"
+    );
+  },
+  displayAchievements: function (data) {
+    const { allAchievements } = data;
+    const { achievementName } = data.name;
+    const { achievementPercentage } = data.percent;
+    document.querySelector(".AchievementsInsert").innerText =
+      achievementName + " " + achievementPercentage;
+  },
+  search: function () {
+    this.fetchAchievements(document.querySelector(".search-bar").value);
+  },
+};
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      achievements.search();
+    }
+  });
+
+achievements.fetchAchievements(440);
 
 function App() {
   return (
@@ -8,12 +39,37 @@ function App() {
       </header>
       <section className="Main">
         <div className="Card">
-          <section id="hero">Image</section>
-          <section id="stats">Stat</section>
-          <section id="users">User</section>
+          <section className="Achievements">
+            <h1>Achievement Percentages</h1>
+            <p>Input the GameId to find Percentages</p>
+            <input
+              type="text"
+              className="search-bar"
+              class="search-bar"
+              placeholder="Search..."
+            />
+            <button className="search" id="search-button">
+              <img
+                id="searchIcon"
+                src="src/assets/images/search.png"
+                alt="search button"
+              />
+            </button>
+            <div className="AchievementsInsert"></div>
+          </section>
+          <section className="stats">Stat</section>
+          <section className="users">User</section>
         </div>
       </section>
-      <footer>Foot</footer>
+      <footer>
+        Foot
+        <a
+          href="https://www.flaticon.com/free-icons/search"
+          title="search icons"
+        >
+          Search icons created by Royyan Wijaya - Flaticon
+        </a>
+      </footer>
     </div>
   );
 }
